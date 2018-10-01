9accb951825ef8ec688fd8628fa6f02c4f6d8787
MonajiLee <lee.smonica@gmail.com>
2018-10-01 21:15:26 +0000

    Correction: Defined empty variable and then added to it in response.on()

--- html-chunks-with-variables.js
+++ html-chunks-with-variables.js
@@ -4,19 +4,20 @@ function getAndPrintHTML () {
 
     var requestOptions = {
       host: 'sytantris.github.io',
-      path: '/http-examples/step1.html'
+      path: '/http-examples/step2.html'
     };
-  
+
+    let datavar = "";
     https.get(requestOptions, function(response){
         
         response.setEncoding('utf8');
         
         response.on('data', function(data){
-            let datavar = data;
-            console.log(datavar);
+            datavar += data;
         });
     
         response.on('end', function(){
+            console.log(datavar);
             console.log('Response stream complete.')
         })
     });
