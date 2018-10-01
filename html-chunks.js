3926538e2b26c0c26e6e6835b7d47836cbc637a1
MonajiLee <lee.smonica@gmail.com>
2018-10-01 18:19:46 +0000

    Prints chunk lengths from HTML path

--- html-chunks.js
+++ html-chunks.js
@@ -1,3 +1,5 @@
+var https = require('https');
+
 function getAndPrintHTMLChunks () {
 
     var requestOptions = {
@@ -5,6 +7,18 @@ function getAndPrintHTMLChunks () {
       path: '/http-examples/step1.html'
     };
   
-    /* Add your code here */
-  
-  }
+    https.get(requestOptions, function(response){
+        
+        response.setEncoding('utf8');
+        
+        response.on('data', function(data){
+            console.log('Chunk receieved. Length:', data.length);
+        });
+    
+        response.on('end', function(){
+            console.log('Response stream complete.')
+        })
+    });
+}
+
+getAndPrintHTMLChunks();
