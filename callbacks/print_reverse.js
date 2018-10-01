15953e33678256293ae7c05c2fd2c5fd9e0c19b1
MonajiLee <lee.smonica@gmail.com>
2018-10-01 19:52:36 +0000

    Converted HTML to array, reversed, and joined together

--- callbacks/print_reverse.js
+++ callbacks/print_reverse.js
@@ -1,1 +1,14 @@
 var getHTML = require('../html-chunks-with-callbacks');
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step6/reverse.html'
+};
+
+function printReverse (html) {
+    let reverseHTML = html.split().reverse().join();
+    console.log(reverseHTML);
+};
+
+// module.exports = function getHTML (options, callback) {}
+getHTML(requestOptions, printReverse);
