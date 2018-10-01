9dba123aa7d0c50b0896ff58ec790592a860f579
MonajiLee <lee.smonica@gmail.com>
2018-10-01 18:25:34 +0000

    Created file and copied getAndPrintHTMLChunks function

---
+++ html-chunks-with-variables.js
@@ -0,0 +1,25 @@
+var https = require('https');
+
+function getAndPrintHTML () {
+
+    var requestOptions = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step1.html'
+    };
+  
+    https.get(requestOptions, function(response){
+        
+        response.setEncoding('utf8');
+        
+        response.on('data', function(data){
+            let datavar = data.length;
+            console.log(datavar);
+        });
+    
+        response.on('end', function(){
+            console.log('Response stream complete.')
+        })
+    });
+}
+
+getAndPrintHTML();
