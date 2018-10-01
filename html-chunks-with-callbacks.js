efcae6c99014a26508d286463f0b365d63df8b05
MonajiLee <lee.smonica@gmail.com>
2018-10-01 18:54:13 +0000

    Accepts callback function, which prints the result

--- html-chunks-with-callbacks.js
+++ html-chunks-with-callbacks.js
@@ -1,10 +1,25 @@
+var https = require('https');
+
 function getHTML (options, callback) {
 
     /* Add your code here */
-  
+    https.get(options, function(response){
+        
+        response.setEncoding('utf8');
+        
+        response.on('data', function(data){
+            let datavar = data;
+            callback(datavar);
+        });
+    
+        response.on('end', function(){
+            console.log('Response stream complete.')
+        })
+    });
   }
   
-  function printHTML (html) {
+  // callback function
+  var printer = function printHTML (html) {
     console.log(html);
   }
   
@@ -12,3 +27,5 @@ function getHTML (options, callback) {
     host: 'sytantris.github.io',
     path: '/http-examples/step4.html'
   };
+
+getHTML(requestOptions, printer);
